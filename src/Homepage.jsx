539e97c302c6ee385126c6203f4d6358d16299dc
Javs C <javcolin1@gmail.com>
2020-12-04 00:11:57 -0800

    changing foler structure

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
-import 'Homepage.scss'
+import './Homepage.scss'
 
 const Homepage = () => {
-	return (
-		<div>
-			
+    return (
+        <div className="homepage" >
+			<div className="directory-menu">
+				<div className="menu-item">
+					<div className="content">
+						<h1 className="title"> HATS </h1>
+						<span className="subtitle"> SHOP NOW </span>
+					</div>					
+				</div>
+
+					<div className="menu-item">
+					<div className="content">
+						<h1 className="title"> SHOES </h1>
+						<span className="subtitle"> SHOP NOW </span>
+					</div>			
+				</div>
+
+					<div className="menu-item">
+					<div className="content">
+						<h1 className="title"> CLOTHES </h1>
+						<span className="subtitle"> SHOP NOW </span>
+					</div>					
+				</div>
+
+					<div className="menu-item">
+					<div className="content">
+						<h1 className="title"> WOMEN </h1>
+						<span className="subtitle"> SHOP NOW </span>
+					</div>					
+				</div>
+				
+					<div className="menu-item">
+					<div className="content">
+						<h1 className="title"> MEN </h1>
+						<span className="subtitle"> SHOP NOW </span>
+					</div>
+					
+				</div>
+				
+				
+
+			</div>
 		</div>
-	)
+    )
 }
 
 export default Homepage

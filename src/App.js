9d644246395bbe262d39860e72a5c4861c7263c3
Javs C <javcolin1@gmail.com>
2020-12-06 23:24:35 -0800

    adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Homepage from './containers/homepage/Homepage'
+import {Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Homepage /> 
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
+
     </div>
   ); 
 }

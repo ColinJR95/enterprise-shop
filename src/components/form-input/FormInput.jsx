d115e73e72a7e2dff8d3e40974faf5b68df2b532
Javs C <javcolin1@gmail.com>
2020-12-20 22:23:14 -0800

    completed formInput component

--- src/components/form-input/FormInput.jsx
+++ src/components/form-input/FormInput.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import './FormInput.scss'
+import './FormInput.scss';
 
-const FormInput = ({handleChange, ...otherProps}) => {
+const FormInput = ({handleChange, label, ...otherProps}) => (
+
 	<div className='group'> 
 		<input className='form-input' onChange={handleChange} {...otherProps} />
 		{label ? (
 			<label className={`${
-				otherProp.value.length ? 'shrink' : ''
+				otherProps.value.length ? 'shrink' : ''
 			} form-input-label`} >
-				{label}
+				{label}	
 			</label>
-			): null}
+			): null
+	}
 	</div>
 
-}
+)
+export default FormInput;
--- src/components/signIn/SignIn.jsx
+++ src/components/signIn/SignIn.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import './SignIn.scss';
+import FormInput from '../form-input/FormInput';
 
 class SignIn extends React.Component {
 	constructor(props) {
@@ -32,22 +34,22 @@ handleChange = event => {
 				<span>Sign in with your email and password</span>
 			
 			<form onSubmit={this.handleSubmit}>
-				<input
+				<FormInput
 				 name="email"
 				 type="email"
 				 value={this.state.email}
-				 onChange={this.handleChange}
+				 handleChange={this.handleChange}
+				 label="email"
 				 required
 				 />
-				<label>Email</label>
-				<input
+				<FormInput
 				name="password" 
 				type="password" 
 				value={this.state.password} 
-				onChange={this.handleChange}
+				handleChange={this.handleChange}
+				label="password"
 				required 
 				/>
-				<label>Password</label>
 				<input type='submit' value='Submit Form' />
 			</form>
 			</div>
